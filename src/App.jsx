6be991c9866367520654c14becea3aef27cc23e8
Shubham Vishwakarma <vi453770@gmail.com>
2025-01-18 19:24:55 +0530

    useState In react

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,43 @@
-import Card from "./component/Card.jsx";
-import "./index.css";
-import Footer from "./component/Footer.jsx";
-import Navbar from "./component/Navbar.jsx";
-function App() {
+// import Card from "./component/Card.jsx";
+// import "./index.css";
+// import Footer from "./component/Footer.jsx";
+// import Navbar from "./component/Navbar.jsx";
+// function App() {
+//   return (
+//     <>
+//       <Navbar />
+//       <div className="card-com">
+//         <Card title="Block1" description="Block 1 Descrption" />
+//         <Card title="Block2" description="Block 2 Descrption" />
+//         <Card title="Block3" description="Block 3 Descrption" />
+//         <Card title="Block4" description="Block 4 Descrption" />
+//         <Card title="Block5" description="Block 5 Descrption" />
+//       </div>
+//       <Footer />
+//     </>
+//   );
+// }
+
+// export default App;
+
+// State and Hooks in REACT
+
+import React from "react";
+import { useState } from "react";
+const App = () => {
+  const [count, setCount] = useState(0);
   return (
-    <>
-      <Navbar />
-      <div className="card-com">
-        <Card title="Block1" description="Block 1 Descrption" />
-        <Card title="Block2" description="Block 2 Descrption" />
-        <Card title="Block3" description="Block 3 Descrption" />
-        <Card title="Block4" description="Block 4 Descrption" />
-        <Card title="Block5" description="Block 5 Descrption" />
-      </div>
-      <Footer />
-    </>
+    <div>
+      <div>{count}</div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Update Count
+      </button>
+    </div>
   );
-}
+};
 
 export default App;
